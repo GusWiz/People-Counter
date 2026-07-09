d1f1fc82b3b0351eb1cb36e9684dad3ed6fa3be5
Gustavo <pow10@txstate.edu>
2026-07-09 10:20:56 -0500

    added js tester file.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+// let count = 0
+
+// console.log(count)
+
+// console.log(myAge)
+
+// let myAge = 22
+
+// console.log(myAge)
+
+// let myAge = 9 
+
+// let humanDogRatio = 7
+
+// myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+let bonusPoints = 50
+
+bonusPoints = bonusPoints + 50
+
+bonusPoints = bonusPoints - 75
+
+bonusPoints = bonusPoints + 45
+
+console.log(bonusPoints)
