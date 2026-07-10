55cd2571323918cc1e3d60a010b9b1ea03176e44
Gustavo <pow10@txstate.edu>
2026-07-10 16:31:58 -0500

    Practice variable creation and logging.

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-// Add event listener
+let firstName = "Gus"
+let lastName = "Her"
 
-// init the count as 0
-// listen for clicks on the increment-btn
-// increment count when clicked
-// update count-el in the HTML to show updated count value
+fullName = firstName + " " + lastName
 
-
-let count = 0
-countEl = document.getElementById("count-el")
-
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
-
-// Working with the DOM is knowon as the Document Object Model, aka how to use
-// js to modify a website.
-
-saveEl = document.getElementById("save-el")
-
-function save() {
-    console.log(count)
-    saveEl.textContent += count + " - "
-    count = 0
-    countEl.textContent = 0
-}
+console.log(fullName)
